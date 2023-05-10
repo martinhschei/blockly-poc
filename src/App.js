90ab4f69a802adc8cf5402a2f87f0d116cffd58e
Martin Hellen Schei <martin.hellen.schei@kd.kongsberg.com>
2023-05-10 15:06:49 +0200

    minors

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PuzzleBoard from './PuzzleBoard.js';
+
+function App() {
+  return (
+    <div className="App">
+      <PuzzleBoard />
+    </div>
+  );
+}
+
+export default App;
